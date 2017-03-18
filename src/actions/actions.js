b172be2f1a602bb0c65b934a99f6bfcb7797ee38
wisheslee <wisheslee@gmail.com>
2017-03-18 23:51:19 +0800

    add actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,8 +1,15 @@
-/**
- * action creator
- */
+
+//constants
+export const REQUEST_POSTS = 'REQUEST_POSTS';
+
+//action creator
+export function requestPosts() {
+  return {
+    type: REQUEST_POSTS
+  }
+}
 export function fetchMenuList() {
   return dispatch => {
-
+    dispatch(requestPosts())
   }
 }
